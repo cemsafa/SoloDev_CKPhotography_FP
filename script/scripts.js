cbbf1c2e84305a886b62ed6c2b414a400a7622fd
Cem Safa <c0820726@mylambton.ca>
2021-12-12 22:29:20 -0500

    Carousel fix

--- script/scripts.js
+++ script/scripts.js
@@ -1,18 +1,28 @@
-var slidePosition = 1;
-showSlides(slidePosition);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-
-//image controls
-function currentSlide(n); {
-
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
-function SlideShow(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dots");
-    if (n > slides.length) {slidePosition = 1}
-    if (n < 1) {slidePosition = slides.length }
-    for ( i=0; i < slides.length; i++) {
-        slides[i].st
-    }
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
